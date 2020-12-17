21e76040d28586e0c7577577b64fb0ba5d3520a7
Balázs Rigó <rigo.balazs86@gmail.com>
2020-12-17 22:01:25 +0100

    Creat Team Form Part 1 Video Done

--- TournamentTracker/TrackerUI/Program.cs
+++ TournamentTracker/TrackerUI/Program.cs
@@ -21,7 +21,7 @@ namespace TrackerUI
             Application.SetCompatibleTextRenderingDefault(false);
 
             GlobalConfig.InitializeConnections(DatabaseType.TextFile);
-            Application.Run(new CreatePrizeForm());
+            Application.Run(new CreateTeamForm());
 
             //Application.Run(new TournamentDashboardForm());
         }
