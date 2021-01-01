5c4a3eb76902bdc6b12b89b58e82f697da955442
Balázs Rigó <rigo.balazs86@gmail.com>
2021-01-01 15:08:50 +0100

    Create Tournament Part 4 Done!

--- TournamentTracker/TrackerLibrary/Models/MatchupEntryModel.cs
+++ TournamentTracker/TrackerLibrary/Models/MatchupEntryModel.cs
@@ -7,28 +7,34 @@ namespace TrackerLibrary.Models
     public class MatchupEntryModel
     {
         /// <summary>
+        /// The unique identifier for the matchup entry.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The unique identifier for the team.
+        /// </summary>
+        public int TeamCompetingId { get; set; }
+
+        /// <summary>
         /// Represents one team in the matchup.
         /// </summary>
         public TeamModel TeamCompeting { get; set; }
 
         /// <summary>
-        /// Represents the score for the team.
+        /// Represents the score for this particular team.
         /// </summary>
         public double Score { get; set; }
+
+        /// <summary>
+        /// The unique identifier for the parent matchup (team).
+        /// </summary>
+        public int ParentMatchupId { get; set; }
 
         /// <summary>
         /// Represents the matchup that this team came 
         /// from as the winner.
         /// </summary>
         public MatchupModel ParentMatchup { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="initialScore"></param>
-        public MatchupEntryModel(double initialScore)
-        {
-
-        }
     }
 }
--- TournamentTracker/TrackerUI/CreateTournamentForm.cs
+++ TournamentTracker/TrackerUI/CreateTournamentForm.cs
@@ -138,6 +138,10 @@ namespace TrackerUI
 
             tm.Prizes = selectedPrizes;
             tm.EnteredTeams = selectedTeams;
+
+            TournamentLogic.CreateRounds(tm);
+
+            GlobalConfig.Connection.CreateTournament(tm);
         }
     }
 }
